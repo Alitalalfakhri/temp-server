f49ddd470e432667242d88e71170991886984868
Alitalalfakhri <ali.talalfakhri09@gmail.com>
2025-04-11 23:53:18 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://3ae136ae-a5de-4351-85cc-4b56963af724-00-1bhn45hk2nnyl.sisko.replit.dev',
+  origin: "https://client-tau-seven.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
 
